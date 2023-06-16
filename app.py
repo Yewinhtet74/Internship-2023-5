0a3584c3387aad6776e01907598bf3b30e7272dd
yewin <yewin04yewin04@gmail.com>
2023-06-16 20:21:21 +0630

    upload

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask,render_template,redirect,request,session
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import or_
+from sqlalchemy import or_,and_
+from sqlalchemy.sql import func
+
 #from flask_migrate import Migrate
 from img2txt import extract_data
-from data import price,payment_images,admin_username,admin_password
-
+from data import price,payment_images,admin_username,admin_password,total_ticket
+from random import randrange
+from PIL import Image
+import os.path
 app = Flask(__name__)
 
 app.secret_key='yewin'
@@ -24,10 +28,15 @@ class Orders(db.Model):
     tickets=db.Column(db.String(200),nullable=False)
     payment=db.Column(db.String(200))
     verify=db.Column(db.Boolean)
-#created_at = db.Column(db.DateTime(timezone=True),server_default=func.now())
+    created_at = db.Column(db.DateTime(timezone=True),default=func.now())
+
+class Ticket_release(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    ticket_no=db.Column(db.String(5))
+    expired=db.Column(db.Boolean)
+
 with app.app_context():
     db.create_all()
-
 
 # route
 @app.route('/')
@@ -38,25 +47,61 @@ def get_session_user():
     if session and session.get('username'):
         return session.get('username')
     return None
-
+def check_release():
+    s=Ticket_release.query.filter_by(expired=None).first()
+    return s
 @app.route('/home')
 def home():  # put application's code here
+    #release_ticket()
+    if check_release():
+        return redirect('/release')
     if get_session_user():
         user=Users.query.filter_by(username=get_session_user()).first()
-        orders = Orders.query.filter(or_(Orders.verify==True , Orders.verify==None))
-        print(orders)
+        orders = Orders.query.filter_by(verify=True)
+        orders2 = Orders.query.filter_by(verify=None)
         tickes={}
-        for i in range(1,151):
+        for i in range(1,total_ticket+1):
             tickes.update({f"{i:03}":1})
         for ord in orders:
             ordered_ticket=ord.tickets.split(',')
-            sts=0
-            if ord.user_id==user.id:    sts=2
+            sts=[0,2][ord.user_id==user.id]
+            for tic in ordered_ticket:
+                tickes[tic]=sts
+        for ord in orders2:
+            ordered_ticket=ord.tickets.split(',')
+            sts=[0,3][ord.user_id==user.id]
             for tic in ordered_ticket:
                 tickes[tic]=sts
         return render_template('home.html',data={'user_session':get_session_user(),'tickes':tickes})
     return render_template('home.html')
 
+@app.route('/release')
+def release():
+    if not(check_release()):
+        return redirect('/home')
+    data={}
+    if get_session_user(): data.update({'user_session':get_session_user()})
+    w=Orders.query.filter(and_(Orders.tickets.contains(check_release().ticket_no),Orders.verify==True))
+    result=None
+    for i in w:result=i
+    if not(result):  data.update({'winner_username':'Unknown'})
+    else:
+        user = Users.query.filter_by(id=result.user_id).first()
+        data.update({'winner_username': user.username})
+    data.update({'ticket_winner':check_release().ticket_no})
+    return render_template('release.html',data=data)
+
+@app.route('/release_ticket')
+def release_ticket():
+    rt=Ticket_release(ticket_no=f"{randrange(0,total_ticket+1):03}")
+    db.session.add(rt)
+    db.session.commit()
+    return redirect('/admin')
+@app.route('/end_release/<id>')
+def end_release(id):
+    rt=Ticket_release.query.filter_by(id=id).update(dict(expired=True))
+    db.session.commit()
+    return redirect('/admin')
 @app.route('/login', methods=['POST','GET'])
 def login():  # put application's code here
     if request.method=='POST':
@@ -117,21 +162,6 @@ def admin_logout():
     session.pop('admin_username',False)
     return redirect('/home')
 
-@app.route('/order',methods=['POST'])
-def order():
-    if get_session_user():
-        u_name=get_session_user()
-        tickets=request.form['tickets']
-        if not(tickets):
-            return redirect('/home')
-        count=len(tickets.split(','))
-        prices=count* price
-        data={'tickets':tickets,'user_session':u_name,'price':price,'quantity':count,'total_price':prices}
-        return render_template('order.html',data=data)
-    else:
-        return redirect('/login')
-from PIL import Image
-import os.path
 def create_filename(name):
     count=0
     tem_name=name
@@ -140,32 +170,48 @@ def create_filename(name):
         tem_name=word[0]+str(count)+'.'+word[1]
         count+=1
     return tem_name
-
-@app.route('/buyticket',methods=['POST'])
-def buyticket():
+import base64,io
+@app.route('/order',methods=['POST'])
+def order():
     if get_session_user():
         u_name=get_session_user()
         tickets=request.form['tickets']
-        file=request.files['file']
-        img=Image.open(file)
-        if file.filename=='' or not(extract_data(img)):
+        if request.form['order_order']:
+            file = request.files['file']
+            '''
+            f=file.read()
+            im=Image.open(io.BytesIO(f))
+            print(im,type(im),extract_data(im))
+            '''
             count = len(tickets.split(','))
             prices = count * price
-            data = {'tickets': tickets, 'user_session': u_name, 'price': price, 'quantity': count,'total_price': prices,'ss_err':'Invalid screenshot! take it clear and try again.'}
-            return render_template('order.html', data=data)
-        #img.close()
-        file_name=create_filename(file.filename)
-        img.save(payment_images+file_name)
-        #file.save(payment_images+file_name)
-        # print(file,extract_data(Image.open(file)))
-        user=Users.query.filter_by(username=u_name).first()
-        order=Orders(user_id=user.id,tickets=tickets,payment=payment_images+file_name)
-        db.session.add(order)
-        db.session.commit()
-        data={'status':'success','tickets':tickets,'user_session':u_name,}
-        return render_template('buy_success.html',data=data)
+            data = {'tickets': tickets, 'user_session': u_name, 'price': price, 'quantity': count,
+                    'total_price': prices}
+            img=Image.open(file)
+            if file.filename == '' or not (extract_data(img)):
+                data.update({ 'ss_err': 'Invalid screenshot! take it clear and try again.'})
+                return render_template('order.html', data=data)
+            file_name = create_filename(file.filename)
+            img.save('static/'+payment_images + file_name)
+            user = Users.query.filter_by(username=u_name).first()
+            order = Orders(user_id=user.id, tickets=tickets, payment=payment_images + file_name)
+            db.session.add(order)
+            db.session.commit()
+            return render_template('buy_success.html', data=data)
+        else:
+            if not(tickets):
+                return redirect('/home')
+            tickets=tickets.split(',')
+            count=len(tickets)
+            prices=count* price
+            tickets.sort()
+            tickets= ','.join(map(str, tickets))
+            data={'tickets':tickets,'user_session':u_name,'price':price,'quantity':count,'total_price':prices}
+            return render_template('order.html',data=data)
     else:
         return redirect('/login')
+
+
 @app.route('/profile')
 def profile():
     if get_session_user():
@@ -192,8 +238,9 @@ def generate_data_for_admin(orders):
         tem['num'] = count;
         count += 1
         tem['price'] = len(tem['tickets'].split(',')) * price
-        img = Image.open(o.payment)
-        tem['payment'] = extract_data(img)
+        img = Image.open('static/'+o.payment)
+        # tem['payment']=img
+        tem['payment_data'] = extract_data(img)
         img.close()
         my_order.append(tem)
     return my_order
@@ -205,22 +252,23 @@ def admin():  # put application's code here
         accept_orders = Orders.query.filter(Orders.verify == True).order_by(Orders.order_id.desc())
         reject_orders = Orders.query.filter(Orders.verify == False).order_by(Orders.order_id.desc())
         data={'admin_username':session.get('admin_username'),
-              'pending_order':generate_data_for_admin(orders),
-              'accepted_order':generate_data_for_admin(accept_orders),
-              'rejected_order':generate_data_for_admin(reject_orders),}
+              'all_orders':{'Pending Order':generate_data_for_admin(orders),
+                            'Accepted Order':generate_data_for_admin(accept_orders),
+                            'Rejected Order':generate_data_for_admin(reject_orders)},}
+        if check_release():
+            data.update({'ticket_winner':check_release().ticket_no,'ticket_winner_id':check_release().id})
         return render_template('admin.html',data=data)
     return redirect('/home')
 
-@app.route('/admin_accept', methods=['POST'])
-def admin_accept():
-    order_id=request.form['order_id']
+@app.route('/admin_accept/<order_id>')
+def admin_accept(order_id):
+    #order_id=request.form['order_id']
     result = Orders.query.filter_by(order_id=order_id).update(dict(verify=True))
     db.session.commit()
     return redirect('/admin')
 
-@app.route('/admin_reject', methods=['POST'])
-def admin_reject():
-    order_id=request.form['order_id']
+@app.route('/admin_reject/<order_id>')
+def admin_reject(order_id):
     result = Orders.query.filter_by(order_id=order_id).update(dict(verify=False))
     db.session.commit()
     return redirect('/admin')
--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 price=1500
 payment_images='payment_images/'
-admin_username='111'
-admin_password='111'
+admin_username='admin'
+admin_password='admin'
+total_ticket=500
